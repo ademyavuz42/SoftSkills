f578fdbd1c96315d4f3276728e077c921b851723
ademyavuz42 <ademyavuz@gmail.com>
2024-03-02 04:47:46 +0700

    Java ilk gun

---
+++ Day01/EscapeSquance.java
@@ -0,0 +1,21 @@
+package Day01;
+
+public class EscapeSquance {
+    public static void main(String[] args) {
+        //\t
+        System.out.println("Hello \tWorld!");
+
+        //\b
+        System.out.println("Hello\bWorld");
+
+        // \n
+        System.out.println("Hello\nWorld");
+
+        // \r
+        System.out.println("Hello\rWorld");
+
+        // \
+        System.out.println("Hello\fWorld");
+
+    }
+}
