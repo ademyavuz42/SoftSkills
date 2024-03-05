1f76f74350b1e2318874a9cb9c89d6113ed3df2a
ademyavuz42 <ademyavuz@gmail.com>
2024-03-05 14:03:24 +0700

    Day 14

---
+++ Day14/PracticeBank/Bank.java
@@ -0,0 +1,10 @@
+package Day14.PracticeBank;
+
+public class Bank {
+    String bankaIsmi = "TuzelBank";
+    String ceoIsmi = "Ankarali Ali";
+    String calisanIsmi = "Bursali Murat";
+    double yillikGelir = 1000000;
+    double yillikGider = 800000;
+
+}
---
+++ Day14/PracticeStudent/Main.java
@@ -0,0 +1,10 @@
+package Day14.PracticeStudent;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MBA mba = new MBA();
+        mba.ogrenciBilgileri("Adem",44,"Good");
+        mba.ogrenciBilgileri("Hafiz",39,"Normal");
+    }
+}
